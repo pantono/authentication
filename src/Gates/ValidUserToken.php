d9ea9c3ae7d1b70893026fff1d7dec92924e90a4
Chris Burton <csburton@gmail.com>
2025-07-26 19:47:18 +0100

    Check JWT token

--- src/Gates/ValidUserToken.php
+++ src/Gates/ValidUserToken.php
@@ -12,18 +12,23 @@ use Symfony\Component\EventDispatcher\EventDispatcher;
 use Pantono\Authentication\Event\UserAuthenticatedEvent;
 use Pantono\Contracts\Endpoint\EndpointDefinitionInterface;
 use Symfony\Component\HttpFoundation\ParameterBag;
+use Pantono\Config\Config;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
 class ValidUserToken implements SecurityGateInterface
 {
     private UserAuthentication $authentication;
     private SecurityContextInterface $securityContext;
     private EventDispatcher $dispatcher;
+    private Config $config;
 
-    public function __construct(UserAuthentication $authentication, SecurityContextInterface $securityContext, EventDispatcher $dispatcher)
+    public function __construct(UserAuthentication $authentication, SecurityContextInterface $securityContext, EventDispatcher $dispatcher, Config $config)
     {
         $this->authentication = $authentication;
         $this->securityContext = $securityContext;
         $this->dispatcher = $dispatcher;
+        $this->config = $config;
     }
 
     public function isValid(Request $request, EndpointDefinitionInterface $endpoint, ParameterBag $options, ?Session $session = null): void
@@ -32,9 +37,17 @@ class ValidUserToken implements SecurityGateInterface
         if (!$tokenString && $session !== null) {
             $tokenString = $session->get('api_token');
         }
+        $jwtUserId = null;
+        $decoded = new \stdClass();
         if (!$tokenString && $request->headers->has('Authorization')) {
             $tokenString = $request->headers->get('Authorization');
             [, $tokenString] = explode(' ', $tokenString, 2);
+            try {
+                $decoded = JWT::decode($tokenString, new Key($this->getJwtSecret(), 'HS256'));
+                $jwtUserId = $decoded->data->user_id;
+            } catch (\Exception $e) {
+                $tokenString = null;
+            }
         }
 
         if (!$tokenString) {
@@ -44,6 +57,10 @@ class ValidUserToken implements SecurityGateInterface
         $token = $this->authentication->getUserTokenByToken($tokenString);
         if ($token === null) {
             throw new AccessDeniedException('User authentication token invalid');
+        }
+
+        if ($jwtUserId !== null && $decoded->data->user_id !== $jwtUserId) {
+            throw new AccessDeniedException('User authentication mismatch');
         }
 
         if ($token->getDateExpires() <= new \DateTime) {
@@ -64,5 +81,24 @@ class ValidUserToken implements SecurityGateInterface
         }
         $event->setSecurityContext($this->securityContext);
         $this->dispatcher->dispatch($event);
+    }
+
+    private function processJwtLogin(Request $request, EndpointDefinitionInterface $endpoint, ParameterBag $options, ?Session $session = null)
+    {
+        $header = $request->headers->get('Authorization');
+        [$type, $token] = explode(' ', $header, 2);
+        if (strtolower($type) === 'bearer') {
+            JWT::$leeway = 60;
+            $decoded = JWT::decode($token, new Key($this->getJwtSecret(), 'HS256'));
+        }
+    }
+
+    private function getJwtSecret(): ?string
+    {
+        $secret = $this->config->getApplicationConfig()->getValue('jwt.secret');
+        if (!$secret) {
+            throw new \RuntimeException('JWT secret not set');
+        }
+        return $secret;
     }
 }
